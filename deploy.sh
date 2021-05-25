74476086534c03b158842c5b50a20083bfb3a1da
Miguel Angel Ajo <majopela@redhat.com>
2021-05-25 10:48:14 +0200

    Update for new image reference and labels
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@
 set -e
 set -x
 
-docker build -t quay.io/mangelajo/pr-brancher-webhook:dev .
-docker push quay.io/mangelajo/pr-brancher-webhook:dev
+docker build -t quay.io/submariner/submariner-bot:dev .
+docker push quay.io/submariner/submariner-bot:dev
 
-kubectl -n pr-brancher-webhook delete pods -l app=pr-brancher
+kubectl -n pr-brancher-webhook delete pods -l app=submariner-bot
 sleep 1
 
 
-while [[ $(kubectl get pods -n pr-brancher-webhook -l app=pr-brancher -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+while [[ $(kubectl get pods -n pr-brancher-webhook -l app=submariner-bot -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
 	echo "waiting for pod" && sleep 1
 done
 kubectl get pods -n pr-brancher-webhook
 
-POD=$(kubectl get pod -n pr-brancher-webhook -l app=pr-brancher -o jsonpath="{.items[0].metadata.name}")
+POD=$(kubectl get pod -n pr-brancher-webhook -l app=submariner-bot -o jsonpath="{.items[0].metadata.name}")
 kubectl logs -n pr-brancher-webhook -f $POD
 
 
