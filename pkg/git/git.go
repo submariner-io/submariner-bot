9066fc3ec2fff507de5588eee9dcf6b180275f58
Tom Pantelis <tompantelis@gmail.com>
2024-01-24 09:18:09 -0500

    Upgrade go-git
    gopkg.in/src-d/go-git.v4 v4.13.1 has a CVE. The repo has been archived and
    continued in github.com/go-git/go-git/v5 so upgrade to that.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -9,12 +9,12 @@ import (
 	"path"
 	"sync"
 
+	gogit "github.com/go-git/go-git/v5"
+	gogitConfig "github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	ssh2 "github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"golang.org/x/crypto/ssh"
-	gogit "gopkg.in/src-d/go-git.v4"
-	gogitConfig "gopkg.in/src-d/go-git.v4/config"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport"
-	ssh2 "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 	"k8s.io/klog"
 
 	"github.com/submariner-io/submariner-bot/pkg/config"
